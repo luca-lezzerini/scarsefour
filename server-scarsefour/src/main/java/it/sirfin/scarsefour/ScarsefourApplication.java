029b3ac438d96e3da7445406e89280e98256006a
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-18 10:39:48 +0100

    Primo commit

---
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/ScarsefourApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.scarsefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScarsefourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScarsefourApplication.class, args);
+	}
+
+}
---
+++ server-scarsefour/src/test/java/it/sirfin/scarsefour/ScarsefourApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.scarsefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarsefourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
