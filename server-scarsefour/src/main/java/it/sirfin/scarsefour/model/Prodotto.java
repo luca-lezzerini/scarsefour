e06790754e84533becb15a86c0cefc3c0006deab
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-18 17:13:43 +0100

    implementate relazioni prodotto-sconti

--- server-scarsefour/src/main/java/it/sirfin/scarsefour/model/Prodotto.java
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/model/Prodotto.java
@@ -7,13 +7,14 @@ package it.sirfin.scarsefour.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Prodotto implements Serializable {
@@ -37,14 +38,14 @@ public class Prodotto implements Serializable {
     @Column
     private int lottoRiordino;
 
-    @JsonIgnoreProperties(value = "cassiera", allowSetters = true)
-    @OneToMany(mappedBy = "cassiera")
-    private Set<Scontrino> scontrini;
+    @JsonIgnoreProperties(value = "prodotto", allowSetters = true)
+    @ManyToMany
+    private Set<Sconto> sconti;
 
     public Prodotto() {
     }
 
-    public Prodotto(String ean, String codice, String descrizione, double prezzo, int scortaMinimaScaf, int scortaMinimaMag, int lottoRiordino) {
+    public Prodotto(String ean, String codice, String descrizione, double prezzo, int scortaMinimaScaf, int scortaMinimaMag, int lottoRiordino, Set<Sconto> sconti) {
         this.ean = ean;
         this.codice = codice;
         this.descrizione = descrizione;
@@ -52,7 +53,87 @@ public class Prodotto implements Serializable {
         this.scortaMinimaScaf = scortaMinimaScaf;
         this.scortaMinimaMag = scortaMinimaMag;
         this.lottoRiordino = lottoRiordino;
+        this.sconti = sconti;
     }
-    
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public int getScortaMinimaScaf() {
+        return scortaMinimaScaf;
+    }
+
+    public void setScortaMinimaScaf(int scortaMinimaScaf) {
+        this.scortaMinimaScaf = scortaMinimaScaf;
+    }
+
+    public int getScortaMinimaMag() {
+        return scortaMinimaMag;
+    }
+
+    public void setScortaMinimaMag(int scortaMinimaMag) {
+        this.scortaMinimaMag = scortaMinimaMag;
+    }
+
+    public int getLottoRiordino() {
+        return lottoRiordino;
+    }
+
+    public void setLottoRiordino(int lottoRiordino) {
+        this.lottoRiordino = lottoRiordino;
+    }
+
+    public Set<Sconto> getSconti() {
+        if (sconti == null) {
+            new ArrayList<>();
+        }
+        return sconti;
+    }
+
+    public void setSconti(Set<Sconto> sconti) {
+        this.sconti = sconti;
+    }
+
+    @Override
+    public String toString() {
+        return "Prodotto{" + "id=" + id + ", ean=" + ean + ", codice=" + codice + ", descrizione=" + descrizione + ", prezzo=" + prezzo + ", scortaMinimaScaf=" + scortaMinimaScaf + ", scortaMinimaMag=" + scortaMinimaMag + ", lottoRiordino=" + lottoRiordino + ", sconti=" + sconti + '}';
+    }
+
 }
