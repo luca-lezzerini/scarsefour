048cadc26c23e263ad8f188ae7c616945a49a102
Giulia2Gargano <giulia2gargano@gmail.com>
2021-03-18 18:24:52 +0100

    Update Cassiera Client

--- client-scarsefour/client-scarsefour/src/app/anagrafica-cassiere/anagrafica-cassiere.component.ts
+++ client-scarsefour/client-scarsefour/src/app/anagrafica-cassiere/anagrafica-cassiere.component.ts
@@ -7,36 +7,37 @@ import { CassieraDto } from '../dto/cassiera-dto';
 import { ListaCassiereDto } from '../dto/lista-cassieri-dto';
 import { Cassiera } from '../entità/cassiera';
 
-
 @Component({
   selector: 'app-anagrafica-cassiere',
   templateUrl: './anagrafica-cassiere.component.html',
   styleUrls: ['./anagrafica-cassiere.component.css']
 })
 export class AnagraficaCassiereComponent implements OnInit, Automabile {
+
   automa: Automa;
+
   cassiera = new Cassiera();
   cassiere: Cassiera[] = [];
-  criterioRicerca = "";
+
+  inputRicerca: string;
+
+  //Variabili di visualizzazione
+  form: boolean;
+  aggiungi: boolean;
+  remove: boolean;
+  edit: boolean;
+  conf: boolean;
+  annull: boolean;
+  search: boolean;
+  tabella: boolean;
+  input: boolean;
+
 
   constructor(private http: HttpClient) {
     this.automa = new Automa(this);
   }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
-
-  form: boolean;
-  aggiungi: boolean = true;
-  remove: boolean;
-  edit: boolean;
-  conf: boolean;
-  annull: boolean;
-  search: boolean = true;
-  tabella: boolean;
-  nomeC: boolean;
-  cognomeC: boolean;
-  codiceFiscaleC: boolean;
 
   nuova() {
     this.automa.next(new AddEvent, this.automa);
@@ -47,8 +48,21 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
   modifica() {
     this.automa.next(new ModificaEvent, this.automa);
   }
+  conferma() {
+    //conferma da che stato???
+    let dto = new CassieraDto();
+    dto.cassiera = this.cassiera;
+    this.http.post<ListaCassiereDto>("http://localhost:8080/add-cassiera", dto)
+      .subscribe(r => {
+        this.cassiere = r.listaCassiere;
+        this.cassiera = new Cassiera();
+      });
+    this.automa.next(new ConfermaEvent, this.automa);
+  }
+
   annulla() {
     this.automa.next(new AnnullaEvent, this.automa);
+
   }
   seleziona() {
     this.automa.next(new SelezionaEvent, this.automa);
@@ -56,24 +70,13 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
   cerca() {
     this.automa.next(new RicercaEvent, this.automa);
   }
-  conferma() {
-    // let dto = new CassieraDto();
-    // dto.cassiera = this.cassiera;
-    // this.http.post<ListaCassiereDto>("http://localhost:8080/add-cassiera", dto)
-    //   .subscribe(r => {
-    //     this.cassiere = r.listaCassiere;
-    //   });
-    this.automa.next(new ConfermaEvent, this.automa);
-  }
 
   entraStatoRicerca() {
     this.form = false;
     this.aggiungi = true;
     this.search = true;
     this.tabella = true;
-    this.nomeC = false;
-    this.cognomeC = false;
-    this.codiceFiscaleC = false;
+    this.input = false;
   }
   entraStatoAggiungi() {
     this.form = true;
@@ -84,10 +87,7 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
     this.annull = true;
     this.search = false;
     this.tabella = false;
-    this.nomeC = false;
-    this.cognomeC = false;
-    this.codiceFiscaleC = false;
-
+    this.input = false;
   }
   entraStatoVisualizza() {
     this.form = true;
@@ -98,11 +98,7 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
     this.annull = false;
     this.search = true;
     this.tabella = true;
-    this.nomeC = true;
-    this.cognomeC = true;
-    this.codiceFiscaleC = true;
-
-
+    this.input = true;
   }
   entraStatoModifica() {
     this.form = true;
@@ -113,10 +109,7 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
     this.annull = true;
     this.search = false;
     this.tabella = false;
-    this.nomeC = false;
-    this.cognomeC = false;
-    this.codiceFiscaleC = false;
-
+    this.input = false;
   }
   entraStatoRimuovi() {
     this.form = true;
@@ -127,10 +120,6 @@ export class AnagraficaCassiereComponent implements OnInit, Automabile {
     this.annull = true;
     this.search = false;
     this.tabella = false;
-    this.nomeC = true;
-    this.cognomeC = true;
-    this.codiceFiscaleC = true;
-
+    this.input = true;
   }
 }
-
