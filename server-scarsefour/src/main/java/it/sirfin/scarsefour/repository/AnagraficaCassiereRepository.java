d438a234e811f5a0e012cba16ce89b6689fb1865
MatteoPalombo99 <78491909+MatteoPalombo99@users.noreply.github.com>
2021-03-19 11:12:42 +0100

    Update query come da ticket #10

--- server-scarsefour/src/main/java/it/sirfin/scarsefour/repository/AnagraficaCassiereRepository.java
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/repository/AnagraficaCassiereRepository.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AnagraficaCassiereRepository extends JpaRepository<Cassiera, Long> {
 
-    @Query("select c from Cassiera c where cognome = :cogn")
+    @Query("select c from Cassiera c where c.nome LIKE CONCAT('%',:cogn,'%') "
+            + "or c.cognome LIKE CONCAT('%',:cogn,'%') or c.codiceFiscale"
+            + "=:cogn")
     List<Cassiera> trovaCassieraPerCognome(@Param("cogn") String cognome);
 }
