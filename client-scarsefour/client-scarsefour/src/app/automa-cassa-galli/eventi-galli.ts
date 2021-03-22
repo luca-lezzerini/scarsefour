4f15d7c26f5bcc81556aa4649ef9a19f9002ac2a
ManuelBava <manuelbava1996@gmail.com>
2021-03-22 16:15:12 +0100

    Update eventi-galli.ts

--- client-scarsefour/client-scarsefour/src/app/automa-cassa-galli/eventi-galli.ts
+++ client-scarsefour/client-scarsefour/src/app/automa-cassa-galli/eventi-galli.ts
@@ -1,9 +1,11 @@
-export interface Event{}
+export interface Event { }
 
-export class VediPrezzoEvent implements Event{}
-export class StornaEvent implements Event{}
-export class ChiudiEvent implements Event{}
-export class ConfermaEvent implements Event{}
-export class AnnullaEvent implements Event{}
-export class AnnullaScontrinoEvent implements Event{}
-export class EanEventGalli implements Event{}
+export class VediPrezzoEvent implements Event { }
+export class StornaEvent implements Event { }
+export class ChiudiEvent implements Event { }
+export class ConfermaEvent implements Event { }
+export class AnnullaEvent implements Event { }
+export class AnnullaScontrinoEvent implements Event { }
+export class EanEventGalli implements Event {
+    ean: string;
+}
