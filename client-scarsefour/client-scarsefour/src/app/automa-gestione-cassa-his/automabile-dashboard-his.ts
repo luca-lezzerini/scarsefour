064d91e1c846b1b2e72e3e894012cef3cbadb822
Matteo Moretti <78532093+matteo2025@users.noreply.github.com>
2021-03-22 16:29:49 +0100

    UC gestione cassa hispanici #15

---
+++ client-scarsefour/client-scarsefour/src/app/automa-gestione-cassa-his/automabile-dashboard-his.ts
@@ -0,0 +1,6 @@
+export interface AutomabileDashboardHis {
+    entraStatoVediPrezzo();
+    entraStatoScontrinoVuoto();
+    entraStatoScontrinoNonVuoto();
+    entraStatoAnnullamentoScontrino();
+}
--- client-scarsefour/client-scarsefour/src/app/dashboard-cassa-his/dashboard-cassa-his.component.ts
+++ client-scarsefour/client-scarsefour/src/app/dashboard-cassa-his/dashboard-cassa-his.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AutomabileDashboardHis } from '../automa-gestione-cassa-his/automabile-dashboard-his';
 
 @Component({
   selector: 'app-dashboard-cassa-his',
   templateUrl: './dashboard-cassa-his.component.html',
-  styleUrls: ['./dashboard-cassa-his.component.css','../theme.css']
+  styleUrls: ['./dashboard-cassa-his.component.css', '../theme.css']
 })
-export class DashboardCassaHisComponent implements OnInit {
+export class DashboardCassaHisComponent implements OnInit, AutomabileDashboardHis {
 
 
   barcode = "";
   descrizioneE = "";
   prezzoE = 0;
   prezzoTot = 0;
+
+  ean:boolean;
+  vediPrezzoVis:boolean;
+  table:boolean;
+  storna:boolean;
+  annullaScontrinoVis:boolean;
+  confermaVis:boolean;
+  annullaVis:boolean;
+  chiudi:boolean;
+  prezzo:boolean;
+  creaNuovoScontrino:boolean;
+
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
@@ -27,6 +42,27 @@ export class DashboardCassaHisComponent implements OnInit {
   }
   tornaUltimo() {
 
+  }
+
+  conferma() {
+
+  }
+
+  annulla() {
+
+  }
+
+  entraStatoVediPrezzo() {
+    throw new Error('Method not implemented.');
+  }
+  entraStatoScontrinoVuoto() {
+
+  }
+  entraStatoScontrinoNonVuoto() {
+    throw new Error('Method not implemented.');
+  }
+  entraStatoAnnullamentoScontrino() {
+    throw new Error('Method not implemented.');
   }
 
 }
