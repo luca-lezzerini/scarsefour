039814974a3499027b08459d3829a4170f70a6bb
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-03-22 19:24:50 +0100

    scheletro component dashboard

--- client-scarsefour/client-scarsefour/src/app/automa-cassa-galli/eventi-galli.ts
+++ client-scarsefour/client-scarsefour/src/app/automa-cassa-galli/eventi-galli.ts
@@ -1,11 +1,21 @@
+import { Scontrino } from "../entità/scontrino";
+
 export interface Event { }
 
 export class VediPrezzoEvent implements Event { }
-export class StornaEvent implements Event { }
+export class StornaEvent implements Event {
+    numeroElementi: number;
+ }
 export class ChiudiEvent implements Event { }
 export class ConfermaEvent implements Event { }
 export class AnnullaEvent implements Event { }
 export class AnnullaScontrinoEvent implements Event { }
 export class EanEventGalli implements Event {
+    constructor(ean: string, scontrino?: Scontrino) {
+        this.ean = ean;
+        this.scontrino = scontrino;
+    }
+
     ean: string;
+    scontrino = new Scontrino();
 }
--- client-scarsefour/client-scarsefour/src/app/dashboard-cassa-galli/dashboard-cassa-galli.component.ts
+++ client-scarsefour/client-scarsefour/src/app/dashboard-cassa-galli/dashboard-cassa-galli.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Automa } from '../automa-cassa-galli/automa';
+import { AutomabileGalli } from '../automa-cassa-galli/automabile-galli';
+import { Scontrino } from '../entità/scontrino';
 import { Prodotto } from '../prodotto';
 
 @Component({
@@ -7,12 +10,32 @@ import { Prodotto } from '../prodotto';
   templateUrl: './dashboard-cassa-galli.component.html',
   styleUrls: ['./dashboard-cassa-galli.component.css']
 })
-export class DashboardCassaGalliComponent implements OnInit {
-
-  constructor(private http: HttpClient) { }
-
+export class DashboardCassaGalliComponent implements OnInit, AutomabileGalli {
   prodotto: Prodotto;
   prodotti: Prodotto[] = [];
+  descrizioneE = "";
+  prezzoE = 0;
+  prezzoTot = 0;
+  automa: Automa;
+
+  ean: boolean;
+  vediPrezzoVis: boolean;
+  table: boolean;
+  storna: boolean;
+  annullaScontrinoVis: boolean;
+  annullaScontrinoEnabled: boolean;
+  confermaVis: boolean;
+  confermaEnabled: boolean;
+  annullaVis: boolean;
+  annullaEnabled: boolean;
+  chiudi: boolean;
+  chiudiEnabled: boolean;
+  prezzo: boolean;
+  scontrino = new Scontrino();
+
+  constructor(private http: HttpClient) {
+    this.automa = new Automa(this);
+  }
 
   ngOnInit(): void {
   }
@@ -23,4 +46,9 @@ export class DashboardCassaGalliComponent implements OnInit {
   annullaScontrino() { }
   annulla() { }
   conferma() { }
+
+  entraStatoScontrinoVuoto(){}
+  entraStatoVediPrezzo(){}
+  entraStatoScontrinoNonVuoto(){}
+  entraStatoAnnullamentoScontrino(){}
 }
