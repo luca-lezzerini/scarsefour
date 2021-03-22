eccebf43f87e4e176ff2a84eb2baad1ea7015db0
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-22 09:16:05 +0100

    Update ScarsefourApplication.java

--- server-scarsefour/src/main/java/it/sirfin/scarsefour/ScarsefourApplication.java
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/ScarsefourApplication.java
@@ -2,7 +2,10 @@ package it.sirfin.scarsefour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Applicazione principale che viene eseguita all'avvio
+ * @author use
+ */
 @SpringBootApplication
 public class ScarsefourApplication {
 
