7f24f9d2ea3ed4130869c8b886f11822609c3aaa
ManuelBava <manuelbava1996@gmail.com>
2021-03-22 13:00:17 +0100

    Create RigaScontrino.java

---
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/model/RigaScontrino.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sirfin.scarsefour.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author manue
+ */
+@Entity
+public class RigaScontrino implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column
+    private Integer quantità;
+
+    public RigaScontrino() {
+    }
+
+    public RigaScontrino(Integer quantità) {
+        this.quantità = quantità;
+    }
+
+}
