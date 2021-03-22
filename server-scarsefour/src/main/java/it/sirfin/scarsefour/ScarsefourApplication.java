60bec57a79228414e62394966370435f18d77975
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-03-22 09:15:10 +0100

    commento app main

--- server-scarsefour/src/main/java/it/sirfin/scarsefour/ScarsefourApplication.java
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/ScarsefourApplication.java
@@ -3,6 +3,10 @@ package it.sirfin.scarsefour;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * programma main per la partenza dell'applicazione (commento Capponi).
+ * @author laptop
+ */
 @SpringBootApplication
 public class ScarsefourApplication {
 
