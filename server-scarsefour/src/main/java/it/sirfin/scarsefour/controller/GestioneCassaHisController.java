b1205ecb31368e637fecd7af52bdb673d5294ca6
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-03-24 13:59:02 +0100

    Update GestioneCassaHisController.java

--- server-scarsefour/src/main/java/it/sirfin/scarsefour/controller/GestioneCassaHisController.java
+++ server-scarsefour/src/main/java/it/sirfin/scarsefour/controller/GestioneCassaHisController.java
@@ -24,6 +24,32 @@ public class GestioneCassaHisController {
     @Autowired
     GestioneCassaHisService gestioneCassaHisService;
 
+    //////////////////////////////////LOGICA/////////////////////////////////////
+//DATI INVIATI DAL CLIENT CON leggiEan(Scontrino(?id), Prodotto.ean)
+//
+//DATI RICEVUTI DAL CLIENT IN RITORNO DAL SERVER(Scontrino, RigaScontrino, messaggio)
+//
+//1)leggi ean (leggi ean significa che il server esegue una query su Db che ritorna il prodotto con l'ean inviato da 
+//client)
+//
+//2)in caso il prodotto con quell'ean non viene trovato il server invia un messaggio di errore (attraverso dto) e il
+//processo termina quà.
+//
+//3)il server controlla se c'è uno scontrino aperto(c'è uno scontrino aperto se il client ne manda uno con id valido
+//e non nullo), se non c'è ne crea uno, ne imposta le proprietà e lo salva su DB.(se c'è questa operazione viene saltata).
+//
+//4)il server esegue una query per trovare se ci sono righe associate allo scontrino.
+//(se non trova nulla) => crea una riga, ne associa il prodotto cercato impostando:
+//	quantita = 1, 
+//	ingnoriamo per ora: MovimentiScaffale.
+//poi associa la riga allo scontrino e infine lo salva su db.
+//
+//(se trova una riga contenente quel prodotto)=> recupera quella riga aggiunge 1 alla quantità e la aggiorna su DB
+//
+//5)aggiorna il totale scontrino su DB
+//
+//6)vengono recuperati tutti i dati necessari al client e inviati tramite dto.
+    
     @RequestMapping("verifica-ean")
     @ResponseBody
     public ProdottoDto verificaEan(@RequestBody EanDtoHis dto) {
