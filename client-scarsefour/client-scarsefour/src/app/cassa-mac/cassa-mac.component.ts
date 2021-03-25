56710b25a60d610dbe9568f3797bf45214c7d3fe
bloodkill23 <78487863+bloodkill23@users.noreply.github.com>
2021-03-25 10:54:09 +0100

    leggere correzioni sul ts

--- client-scarsefour/client-scarsefour/src/app/cassa-mac/cassa-mac.component.ts
+++ client-scarsefour/client-scarsefour/src/app/cassa-mac/cassa-mac.component.ts
@@ -9,6 +9,8 @@ import { Scontrino } from '../entità/scontrino';
 import { Prodotto } from '../prodotto';
 import { RigaScontrino } from '../entità/riga-scontrino';
 import { ProdottoDto } from '../dto/prodotto-dto';
+import { LeggiEanRequestDto } from '../dto/leggi-ean-request-dto';
+import { LeggiEanResponseDto } from '../dto/leggi-ean-response-dto';
 
 @Component({
   selector: 'app-cassa-mac',
@@ -81,7 +83,24 @@ export class CassaMacComponent implements OnInit, AutomabileDashboardMac {
     this.automaCassa.next(new AnnullaEvent, this.automaCassa);
   }
 
+  entraStatoScontrinoVuoto() {
+    this.ean = true;
+    this.vediPrezzoV = true;
+    this.listaScontrino = false;
+    this.storna = false;
+    this.annullaScontrinoV = true;
+    this.confermaV = true;
+    this.annullaV = true;
+    this.chiudi = true;
+    this.prezzo = false;
+    this.annullaScontrinoEnabled = true;
+    this.confermaEnabled = true;
+    this.annullaEnabled = true;
+    this.chiudiEnabled = true;
+    this.errore = false;
 
+    //revisionato
+  }
   entraStatoVediPrezzo() {
     this.ean = true;
     this.vediPrezzoV = false;
@@ -98,26 +117,8 @@ export class CassaMacComponent implements OnInit, AutomabileDashboardMac {
     this.chiudiEnabled = false;
     this.errore = false;
 
-
   }
-  entraStatoScontrinoVuoto() {
-    this.ean = true;
-    this.vediPrezzoV = true;
-    this.listaScontrino = false;
-    this.storna = false;
-    this.annullaScontrinoV = true;
-    this.confermaV = false;
-    this.annullaV = false;
-    this.chiudi = true;
-    this.prezzo = false;
-    this.annullaScontrinoEnabled = true;
-    this.confermaEnabled = false;
-    this.annullaEnabled = false;
-    this.chiudiEnabled = true;
-    this.errore = false;
 
-    //revisionato
-  }
   entraStatoScontrinoVuotoDaVediPrezzo() {
     //ean conosciuto
     this.ean = true;
@@ -248,17 +249,26 @@ export class CassaMacComponent implements OnInit, AutomabileDashboardMac {
 
 
   ricercaEan() {
-    let dto = new RicercaProdottoDto();
-    dto.ricercaPerCodice = this.barcode;
-    if (this.barcode == null) {
-      console.log("prodotto non trovato!");
+    let dto = new LeggiEanRequestDto();
+    dto.eanProdotto = this.barcode;
+    dto.scontrino = this.scontrino;
+   /* if (this.barcode == null) {
+      //console.log("prodotto non trovato!");
     } else {
       this.http.post<ListaProdottiDto>("http://localhost:8080/ricerca-prodotto-mac", dto)
         .subscribe(k => {
           this.prodotti = k.listaProdotti;
           this.barcode = "";
         });
-    }
+    }*/
+  
+    this.http.post<LeggiEanResponseDto>("http://localhost:8080/ricerca-prodotto-mac", dto)
+    .subscribe(a => {
+      this.scontrino = a.scontrino;
+      this.righeScontrino = a.righeScontrino;
+      this.barcode = "";
+    });
+  
   }
 
 
